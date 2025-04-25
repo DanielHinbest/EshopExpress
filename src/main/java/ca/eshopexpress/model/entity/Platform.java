fb7063db5149c795344c200b1c67adf9ebfaad2b
Daniel Hinbest <60987457+DanielHinbest@users.noreply.github.com>
2025-04-24 22:32:03 -0400

    Adjusted Platform title to require a unique entry

--- src/main/java/ca/eshopexpress/model/entity/Platform.java
+++ src/main/java/ca/eshopexpress/model/entity/Platform.java
@@ -23,7 +23,7 @@ public class Platform {
     /**
      * The platform name
      */
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     /**
