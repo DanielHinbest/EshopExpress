06c4153a4963ed96f424304c5d052a26e98feae2
Daniel Hinbest <60987457+DanielHinbest@users.noreply.github.com>
2025-05-09 09:54:58 -0400

    Fixed error in AgeRating.java

--- src/main/java/ca/eshopexpress/model/enums/AgeRating.java
+++ src/main/java/ca/eshopexpress/model/enums/AgeRating.java
@@ -1,4 +1,19 @@
 package ca.eshopexpress.model.enums;
 
 public enum AgeRating {
+    EVERYONE("E", "Everyone"),
+    EVERYONE_TEN_PLUS("E10+", "Everyone 10+"),
+    TEEN("T", "Teen"),
+    MATURE("M17+", "Mature 17+"),
+    ADULTS_ONLY("AO", "Adults Only 18+"),
+    RATING_PENDING("RP", "Rating Pending"),
+    RATING_PENDING_17_PLUS("RP17+", "Rating Pending - Likely Mature 17+");
+
+    private final String code;
+    private final String description;
+
+    AgeRating(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
 }
